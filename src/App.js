e34fb5851393825db3bc1786c22d2033e2ec298f
ZDahir <zaidmdahir@gmail.com>
2024-11-05 22:59:29 -0500

    renamed image name

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+// App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import styled from 'styled-components';
-import Home from './pages/Home/Home';
-import TidyCalEmbed from './components/TidyCalEmbed';
+import Home from './pages/Home/Home'; // Import the Home component
+import TidyCalEmbed from './components/TidyCalEmbed'; // Import the TidyCalEmbed component
 import Resume from './components/Resume';
 
 function App() {
@@ -13,20 +14,21 @@ function App() {
           {/* Home Page */}
           <Route path="/" element={<Home />} />
 
-          {/* /book Page with Styled EmbedBackground and EmbedContainer */}
-          <Route
-            path="/book"
-            element={
-              <EmbedBackground>
-                <EmbedContainer>
-                  <TidyCalEmbed />
-                </EmbedContainer>
-              </EmbedBackground>
-            }
-          />
+          {/* /book Page */}
+          <Route path="/book" element={<TidyCalEmbed />} />
+        
+        {/* Resume Page */}
+        <Route
+  path="/book"
+  element={
+    <EmbedBackground>
+      <EmbedContainer>
+        <TidyCalEmbed />
+      </EmbedContainer>
+    </EmbedBackground>
+  }
+/>
 
-          {/* Resume Page */}
-          <Route path="/resume" element={<Resume />} />
         </Routes>
       </AppWrapper>
     </Router>
@@ -41,6 +43,7 @@ const AppWrapper = styled.div`
   height: 100vh;
 `;
 
+
 // /book Page Styling
 const EmbedBackground = styled.div`
   background-color: #ffffff;
@@ -49,12 +52,12 @@ const EmbedBackground = styled.div`
   align-items: center;
   height: 100vh;
 `;
-
 const EmbedContainer = styled.div`
-  width: 100%;
-  max-width: 800px;
-  padding: 20px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  background-color: #fff;
-  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* No additional styles that might interfere */
 `;
+
+
+
